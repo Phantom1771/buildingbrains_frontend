38e4dd6bd992c6816bf5a9a16933f27efab7c518
Pezhin <p@oath.pl>
2017-02-06 18:12:33 -0700

    fancy demo page for week2 meeting

--- Standalone_Homepage/src/Nav.js
+++ Standalone_Homepage/src/Nav.js
@@ -10,15 +10,39 @@ class Nav extends Component {
         <div className="col-sm-3 col-md-2 sidebar">
           <ul className="nav nav-sidebar">
             <li className="active "><a href="#">  
-              <span className="glyphicon glyphicon-home"></span> Home
+              <i className="fa fa-home fa-3x" aria-hidden="true"></i>
+              <p>Home</p>
             </a></li>
           </ul>
+
           <ul className="nav nav-sidebar">
-            <li><a href="">Nav item</a></li>
-            <li><a href="">Nav item again</a></li>
-            <li><a href="">One more nav</a></li>
-            <li><a href="">One more nav</a></li>
+            <li><a href="">
+              <i className="fa fa-object-group fa-3x" aria-hidden="true"></i>
+              <p>Devices</p>
+              </a></li>
           </ul>
+
+          <ul className="nav nav-sidebar">
+            <li><a href="">
+              <i className="fa fa-random fa-3x" aria-hidden="true"></i>
+              <p>Stats</p>
+              </a></li>
+          </ul>
+
+          <ul className="nav nav-sidebar">
+            <li><a href="">
+              <i className="fa fa-users fa-3x" aria-hidden="true"></i>
+              <p>User</p>
+              </a></li>
+          </ul>
+
+          <ul className="nav nav-sidebar">
+            <li><a href="">
+              <i className="fa fa-cogs fa-3x" aria-hidden="true"></i>
+              <p>Settings</p>
+              </a></li>
+          </ul>
+            
         </div>
         </div>
         </div>
