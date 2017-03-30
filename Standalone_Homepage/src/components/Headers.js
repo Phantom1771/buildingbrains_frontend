517a74ec23b00f7685ba4f9c266232ebbe8ff61e
Pezhin <p@oath.pl>
2017-03-30 16:59:39 -0600

    headers update

--- Standalone_Homepage/src/components/Headers.js
+++ Standalone_Homepage/src/components/Headers.js
@@ -9,15 +9,15 @@ class Headers extends Component {
       <nav className="navbar navbar-inverse navbar-fixed-top">
       <div className="container-fluid">
         <div className="navbar-header">
-          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
-            <span className="sr-only">Toggle navigation</span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-          </button>
+          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+						<span className="sr-only">Toggle navigation</span>
+						<span className="icon-bar"></span>
+						<span className="icon-bar"></span>
+						<span className="icon-bar"></span>
+					</button>
           <a className="navbar-brand" href="#">Project name</a>
         </div>
-        <div id="navbar" className="collapse">
+        <div id="navbar" className="navbar-collapse collapse">
             {Auth.loggedIn()?
               <div>
               <ul className="nav navbar-nav navbar-right">
@@ -28,7 +28,12 @@ class Headers extends Component {
                 <li><a href="/Automation">Automation</a></li>
                 <li><Link onClick={Auth.logout.bind(this)}>Log out</Link></li>
               </ul>
-              </div>:<li><Link to="/Login">Log in</Link></li>}
+              </div>:
+              <div>
+                <ul className="nav navbar-nav navbar-right">
+                  <li><Link to="/Login">Log in</Link></li>
+                </ul>
+              </div>}
         </div>
         
       </div>
